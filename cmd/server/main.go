b3cbfafdbb57b41b13dcac90c975e38029602483
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2025-10-06 11:47:57 +0300

    feat(main): add building the app
    Added initialization:
    1. metrics repo, service, handler;
    2. mux;
    3. /update/{type}/{name}/{value} url;
    4. server.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"github.com/iPatrushevSergey/metrics/internal/handler"
+	"github.com/iPatrushevSergey/metrics/internal/repository/inmemory"
+	"github.com/iPatrushevSergey/metrics/internal/service"
+)
+
+func main() {
+	repo := inmemory.NewMemStorageMetricRepository()
+	metricService := service.NewMetricService(repo)
+	metricHandler := handler.NewMetricHandler(metricService)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/update/{type}/{name}/{value}`, metricHandler.Update)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+}
