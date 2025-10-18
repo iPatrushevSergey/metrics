80cb326d37053284839e8d9b433b1719c6bdc5ee
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2025-10-18 21:38:36 +0300

    feat(tests): add service tests
    Added TestMetricServiceUpdate tests:
    1. create Gauge;
    2. update Gauge;
    3. create Counter;
    4. update Counter.

---
+++ internal/service/metric_test.go
@@ -0,0 +1,99 @@
+package service
+
+import (
+	"testing"
+
+	"github.com/iPatrushevSergey/metrics/internal/model"
+	"github.com/iPatrushevSergey/metrics/internal/repository/inmemory"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMetricServiceUpdate(t *testing.T) {
+	floatp := func(f float64) *float64 { return &f }
+	intp := func(i int64) *int64 { return &i }
+
+	type want struct {
+		metric model.Metric
+		exists bool
+	}
+
+	tests := []struct {
+		name         string
+		initialState map[string]model.Metric
+		metricType   string
+		metricName   string
+		metricValue  any
+		want         want
+	}{
+		{
+			name:         "create Gauge",
+			initialState: map[string]model.Metric{},
+			metricType:   model.Gauge,
+			metricName:   "newGauge",
+			metricValue:  float64(10.3),
+			want: want{
+				metric: model.Metric{MType: model.Gauge, Value: floatp(10.3)},
+				exists: true,
+			},
+		},
+		{
+			name: "update Gauge",
+			initialState: map[string]model.Metric{
+				"existingGauge": {ID: "uuid1", MType: model.Gauge, Value: floatp(9.1)},
+			},
+			metricType:  model.Gauge,
+			metricName:  "existingGauge",
+			metricValue: float64(2.1),
+			want: want{
+				metric: model.Metric{ID: "uuid1", MType: model.Gauge, Value: floatp(2.1)},
+				exists: true,
+			},
+		},
+		{
+			name:         "create Counter",
+			initialState: map[string]model.Metric{},
+			metricType:   model.Counter,
+			metricName:   "newCounter",
+			metricValue:  int64(10),
+			want: want{
+				metric: model.Metric{MType: model.Counter, Delta: intp(10)},
+				exists: true,
+			},
+		},
+		{
+			name: "update Counter",
+			initialState: map[string]model.Metric{
+				"existingCounter": {ID: "uuid1", MType: model.Counter, Delta: intp(10)},
+			},
+			metricType:  model.Counter,
+			metricName:  "existingCounter",
+			metricValue: int64(10),
+			want: want{
+				metric: model.Metric{ID: "uuid1", MType: model.Counter, Delta: intp(20)},
+				exists: true,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockRepo := inmemory.NewMemStorageMetricRepository()
+			typedMockRepo, ok := mockRepo.(*inmemory.MemStorageMetricRepository)
+			require.True(t, ok, "Failed to cast mockRepo to *MemStorageMetricRepository")
+
+			typedMockRepo.DB = tt.initialState
+
+			service := NewMetricService(mockRepo)
+			err := service.Update(tt.metricType, tt.metricName, tt.metricValue)
+			require.NoError(t, err)
+
+			resultMetric, exists := mockRepo.GetByName(tt.metricName)
+
+			require.Equal(t, tt.want.exists, exists)
+
+			assert.Equal(t, tt.want.metric.MType, resultMetric.MType)
+			assert.Equal(t, tt.want.metric.Value, resultMetric.Value)
+			assert.Equal(t, tt.want.metric.Delta, resultMetric.Delta)
+		})
+	}
+}
