1bb3b90b2de03316aa1fd3d044cbef6f7edea6c2
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2025-10-18 21:42:57 +0300

    feat(main): add graceful shutdown
    Added pattern "Graceful Shutdown".

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"context"
+	"errors"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/iPatrushevSergey/metrics/internal/handler"
 	"github.com/iPatrushevSergey/metrics/internal/repository/inmemory"
@@ -14,10 +21,34 @@ func main() {
 	metricHandler := handler.NewMetricHandler(metricService)
 
 	mux := http.NewServeMux()
-	mux.HandleFunc(`/update/{type}/{name}/{value}`, metricHandler.Update)
+	mux.HandleFunc(`POST /update/{type}/{name}/{value}`, metricHandler.Update)
 
-	err := http.ListenAndServe(`:8080`, mux)
-	if err != nil {
-		panic(err)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
 	}
+
+	go func() {
+		log.Printf("Starting server, address: %s", server.Addr)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Printf("Server failed to start, error: %v", err)
+			os.Exit(1)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("The completion signal has been received, starting the stop...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	log.Println("Shutting down server...")
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Server shutdown failed, error: %v", err)
+		os.Exit(1)
+	}
+
+	log.Println("Server stopped gracefully")
 }
