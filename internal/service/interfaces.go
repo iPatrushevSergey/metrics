4818f42ac7d8e999d3b2cf6cdaf9bfc7155e2fc0
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2025-10-19 11:50:35 +0300

    build(mod): update the go mod
    Updated go.mod and go.sum.

--- internal/service/interfaces.go
+++
@@ -1,9 +0,0 @@
-package service
-
-import "github.com/iPatrushevSergey/metrics/internal/model"
-
-type MetricRepository interface {
-	GetByName(name string) (model.Metric, bool)
-	Update(name string, metric model.Metric)
-	Create(name string, metric model.Metric)
-}
