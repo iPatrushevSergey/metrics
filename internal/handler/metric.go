9770fdc5e33cf1d88ab2499995e338262e879c23
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2025-10-28 22:45:14 +0300

    refactor(handlers): eliminate the review's comments
    All validation, string normalization, and parsing
    have been moved to the service layer.

--- internal/handler/metric.go
+++ internal/handler/metric.go
@@ -1,17 +1,14 @@
 package handler
 
 import (
-	"fmt"
+	"errors"
 	"log"
 	"net/http"
-	"sort"
-	"strconv"
 	"strings"
 
 	"html/template"
 
 	"github.com/gin-gonic/gin"
-	"github.com/iPatrushevSergey/metrics/internal/model"
 	"github.com/iPatrushevSergey/metrics/internal/service"
 )
 
@@ -31,11 +28,6 @@ const metricsHTMLTemplate = `
 
 var metricsTemplate = template.Must(template.New("metrics").Parse(metricsHTMLTemplate))
 
-type templateData struct {
-	Name  string
-	Value string
-}
-
 type MetricHandler struct {
 	metricService *service.MetricsService
 }
@@ -44,114 +36,63 @@ func NewMetricHandler(s *service.MetricsService) *MetricHandler {
 	return &MetricHandler{metricService: s}
 }
 
-func formatMetricValue(metric model.Metric) (string, error) {
-	switch metric.MType {
-	case model.Gauge:
-		if metric.Value == nil {
-			return "", fmt.Errorf("gauge value is nil")
-		}
-		return strconv.FormatFloat(*metric.Value, 'f', -1, 64), nil
-	case model.Counter:
-		if metric.Delta == nil {
-			return "", fmt.Errorf("counter value is nil")
-		}
-		return strconv.FormatInt(*metric.Delta, 10), nil
-	default:
-		return "", fmt.Errorf("unknown metric MType: %s", metric.MType)
-	}
-}
-
 func (h *MetricHandler) Get(c *gin.Context) {
-	metricType := strings.ToLower(c.Param("type"))
-	metricName := strings.TrimSpace(strings.ToLower(c.Param("name")))
+	metricType := c.Param("type")
+	metricName := c.Param("name")
 
-	switch metricType {
-	case model.Gauge, model.Counter:
-		metric, err := h.metricService.Get(metricName)
-		if err != nil {
+	metric, err := h.metricService.Get(metricType, metricName)
+	if err != nil {
+		if errors.Is(err, service.ErrNotFound) {
 			c.String(http.StatusNotFound, err.Error())
-			return
+		} else if errors.Is(err, service.ErrBadMetricType) {
+			c.String(http.StatusBadRequest, err.Error())
+		} else {
+			log.Printf("Internal server error in Get: %v", err)
+			c.String(http.StatusInternalServerError, service.ErrInternal.Error())
 		}
-
-		value, err := formatMetricValue(metric)
-		if err != nil {
-			c.String(http.StatusInternalServerError, err.Error())
-			return
-		}
-
-		c.String(http.StatusOK, "%s", value)
-	default:
-		c.String(http.StatusBadRequest, "invalid metric type")
 		return
 	}
+
+	c.String(http.StatusOK, "%s", metric)
 }
 
 func (h *MetricHandler) GetAll(c *gin.Context) {
-	metrics := h.metricService.GetAll()
-
-	keys := make([]string, 0, len(metrics))
-	for key := range metrics {
-		keys = append(keys, key)
+	metrics, err := h.metricService.GetAll()
+	if err != nil {
+		log.Printf("Internal server error in GetAll: %v", err)
+		c.String(http.StatusInternalServerError, "Failed to get metrics")
+		return
 	}
-	sort.Strings(keys)
 
-	data := struct {
-		Metrics []templateData
-	}{}
-
-	for _, key := range keys {
-		value, err := formatMetricValue(metrics[key])
-		if err != nil {
-			log.Printf("error formatting metric %s: %v", key, err)
-			continue
-		}
-		data.Metrics = append(data.Metrics, templateData{Name: key, Value: value})
-	}
 	c.Header("Content-Type", "text/html; charset=utf-8")
 
-	err := metricsTemplate.Execute(c.Writer, data)
+	err = metricsTemplate.Execute(c.Writer, metrics)
 	if err != nil {
+		log.Printf("Internal server error rendering template: %v", err)
 		c.String(http.StatusInternalServerError, "Failed to render page")
 	}
 }
 
 func (h *MetricHandler) Update(c *gin.Context) {
-	// Transport validation
-	metricType := strings.ToLower(c.Param("type"))
-	metricName := strings.TrimSpace(strings.ToLower(c.Param("name")))
-	metricValue := strings.ToLower(c.Param("value"))
+	metricType := c.Param("type")
+	metricName := c.Param("name")
+	metricValue := c.Param("value")
 
-	if metricName == "" {
+	// Это транспортная валидация. Ее стоит тоже перенести в сервисный слой
+	// или можно оставить в хэндлере?
+	if strings.TrimSpace(metricName) == "" {
 		c.String(http.StatusNotFound, "The metric name is missing")
 		return
 	}
 
-	// Format validation, parsing, update
-	switch metricType {
-	case model.Gauge:
-		val, err := strconv.ParseFloat(metricValue, 64)
-		if err != nil {
-			c.String(http.StatusBadRequest, "Invalid gauge value")
-			return
+	err := h.metricService.Update(metricType, metricName, metricValue)
+	if err != nil {
+		if errors.Is(err, service.ErrBadMetricType) || errors.Is(err, service.ErrBadMetricValue) {
+			c.String(http.StatusBadRequest, err.Error())
+		} else {
+			log.Printf("Internal server error in Update: %v", err)
+			c.String(http.StatusInternalServerError, service.ErrInternal.Error())
 		}
-		err = h.metricService.Update(metricType, metricName, val)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "failed to update metric")
-			return
-		}
-	case model.Counter:
-		val, err := strconv.ParseInt(metricValue, 10, 64)
-		if err != nil {
-			c.String(http.StatusBadRequest, "Invalid counter value")
-			return
-		}
-		err = h.metricService.Update(metricType, metricName, val)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "failed to update metric")
-			return
-		}
-	default:
-		c.String(http.StatusBadRequest, "Invalid metric type")
 		return
 	}
 
