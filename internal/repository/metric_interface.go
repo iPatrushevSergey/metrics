6ab99398cdeacc76a61a43afe7c0777affb8efd1
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2025-11-12 00:10:09 +0300

    refactor(interfaces): change metric interface
    The signature MetricRepository methods has been changed.

--- internal/repository/metric_interface.go
+++ internal/repository/metric_interface.go
@@ -3,8 +3,8 @@ package repository
 import "github.com/iPatrushevSergey/metrics/internal/model"
 
 type MetricRepository interface {
-	GetByName(name string) (model.Metric, bool)
+	GetByID(id string) (model.Metric, bool)
 	GetAll() map[string]model.Metric
-	Update(name string, metric model.Metric)
-	Create(name string, metric model.Metric)
+	Update(id string, metric model.Metric)
+	Create(metric model.Metric)
 }
