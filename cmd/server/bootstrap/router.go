c348dc43ddcdf0b76562034998e2d9222c18b5c6
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2025-12-28 15:51:39 +0300

    refactor(server): remove commented code
    Removed commented RedirectTrailingSlash and path normalization middleware code

--- cmd/server/bootstrap/router.go
+++ cmd/server/bootstrap/router.go
@@ -22,15 +22,7 @@ func (g *GinJSONSerializer) Deserialize(c *gin.Context, data []byte, v interface
 // SetupRouter configures and returns the HTTP router with all routes and middleware
 func SetupRouter(metricHandler *handler.MetricHandler, cfg config.ServerConfig) *gin.Engine {
 	router := gin.New()
-	// router.RedirectTrailingSlash = false
 	router.Use(gin.Recovery())
-	// router.Use(func(c *gin.Context) {
-	// 	path := c.Request.URL.Path
-	// 	if path != "/" && strings.HasSuffix(path, "/") && !strings.Contains(path, ":") {
-	// 		c.Request.URL.Path = strings.TrimSuffix(path, "/")
-	// 	}
-	// 	c.Next()
-	// })
 	router.Use(middleware.GzipGinMiddleware())
 	router.Use(middleware.HashMiddleware(cfg.Key))
 	router.Use(middleware.LoggerMiddleware())
