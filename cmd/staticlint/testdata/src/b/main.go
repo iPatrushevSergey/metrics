e1284496d6632f63439c4be7e01b12505f0939b2
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-03-29 14:26:46 +0300

    test(staticlint): drop old testdata package b
    Remove duplicate main-only case before replacing b with a library package.

--- cmd/staticlint/testdata/src/b/main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import o "os"
-
-// direct os.Exit via import alias in package main func main: must be reported.
-func main() {
-	o.Exit(2) // want "direct os.Exit call in main is forbidden"
-}
