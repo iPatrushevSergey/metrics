eb28ed8ecc873d1f2c53997649ee06b646758f46
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-03-29 15:57:56 +0300

    test(staticlint): expect panic in non-main package b
    Assert panic is reported alongside exit/fatal outside real main.

--- cmd/staticlint/testdata/src/b/b.go
+++ cmd/staticlint/testdata/src/b/b.go
@@ -5,4 +5,5 @@ import "os"
 // Not package main: func main here is a normal function, not exempt.
 func main() {
 	os.Exit(1) // want "forbidden: os.Exit or log.Fatal outside func main of package main"
+	panic("x") // want "forbidden: built-in panic"
 }
