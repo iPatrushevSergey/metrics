fb37a0a86b6f0650969e8d414e926393b213c861
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-03-29 14:28:30 +0300

    test(staticlint): add non-main package b testdata
    Expect a report when os.Exit appears outside real package main.

---
+++ cmd/staticlint/testdata/src/b/b.go
@@ -0,0 +1,8 @@
+package b
+
+import "os"
+
+// Not package main: func main here is a normal function, not exempt.
+func main() {
+	os.Exit(1) // want "forbidden: os.Exit or log.Fatal outside func main of package main"
+}
