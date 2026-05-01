47d984f7b2751056d2aa3dc821a1857de6cff350
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-01 19:35:40 +0300

    refactor(agent|port): Refact Encryptor port
    Renamed Encrypt method to Seal method.

--- metrics_new/app/internal/agent/collector/adapters/metrics_client/ports/encryptor.go
+++ metrics_new/app/internal/agent/collector/adapters/metrics_client/ports/encryptor.go
@@ -2,5 +2,5 @@ package ports
 
 // Encryptor interface for data encryption.
 type Encryptor interface {
-	Encrypt(plaintext []byte) (payload []byte, contentType string, headers map[string]string, err error)
+	Seal(plaintext []byte) (payload []byte, contentType string, headers map[string]string, err error)
 }
