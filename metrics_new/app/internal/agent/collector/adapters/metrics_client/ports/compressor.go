22e7a7a7223d419fbf6fc65675bb94c89871d9eb
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-01 19:53:19 +0300

    refactor(agent|port): change the names of Compressor methods
    Compressor methods have been renamed to ContentEncoding, DecompressReader, CompressWriter.

--- metrics_new/app/internal/agent/collector/adapters/metrics_client/ports/compressor.go
+++ metrics_new/app/internal/agent/collector/adapters/metrics_client/ports/compressor.go
@@ -5,6 +5,6 @@ import "io"
 // Compressor interface for data compression.
 type Compressor interface {
 	ContentEncoding() string
-	NewReader(r io.Reader) (io.ReadCloser, error)
-	NewWriter(w io.Writer) io.WriteCloser
+	DecompressReader(r io.Reader) (io.ReadCloser, error)
+	CompressWriter(w io.Writer) io.WriteCloser
 }
