2b8cb218ef74339d7e24d6afa517f17963287a4f
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-09 21:21:37 +0300

    feat(server|middleware): add default HTTP log formatter
    Log URI, method, duration, status and size at info; log request and response bodies at debug.

---
+++ metrics_new/app/internal/pkg/presentation/http/middleware/logger/default_formatter.go
@@ -0,0 +1,20 @@
+package logger
+
+import "github.com/iPatrushevSergey/metrics/metrics_new/app/internal/pkg/presentation/http/port"
+
+// DefaultLogFormatter implements standard logging logic.
+type DefaultLogFormatter struct{}
+
+func (f *DefaultLogFormatter) Log(log port.Logger, p LogParams) {
+	log.Info("HTTP request",
+		"uri", p.Ctx.Request.RequestURI,
+		"method", p.Ctx.Request.Method,
+		"duration", p.Duration,
+		"status", p.Ctx.Writer.Status(),
+		"size", p.Ctx.Writer.Size(),
+	)
+	log.Debug("HTTP request/response body",
+		"request_body", string(p.RequestBody),
+		"response_body", p.ResponseBody.String(),
+	)
+}
