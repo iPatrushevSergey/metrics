56de2afbdcce7caba32d9e2675d047ccb1ca2601
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-13 20:37:14 +0300

    refactor(agent|adapters): update comment

--- metrics_new/app/internal/agent/collector/adapters/metrics_gateway/config.go
+++ metrics_new/app/internal/agent/collector/adapters/metrics_gateway/config.go
@@ -2,7 +2,7 @@ package metrics_gateway
 
 import "time"
 
-// GatewayConfig holds HTTP transport settings for the metrics Gateway.
+// MetricsGatewayConfig holds HTTP transport settings for the metrics gateway.
 type MetricsGatewayConfig struct {
 	Address     string        `mapstructure:"address"`
 	HTTPTimeout time.Duration `mapstructure:"http_timeout"`
