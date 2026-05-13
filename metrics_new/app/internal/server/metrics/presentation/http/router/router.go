02c9ddd534b3ed22020a4e2e7087e3de63397c53
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-13 20:45:34 +0300

    feat(server|presentation): add metric router
    Added RegisterRouter for metrics.

---
+++ metrics_new/app/internal/server/metrics/presentation/http/router/router.go
@@ -0,0 +1,20 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+
+	"github.com/iPatrushevSergey/metrics/metrics_new/app/internal/server/metrics/application/port"
+	"github.com/iPatrushevSergey/metrics/metrics_new/app/internal/server/metrics/presentation/http/handler"
+)
+
+// RegisterRoutes registers metrics endpoints on the router.
+func RegisterRoutes(r *gin.Engine, ucFactory handler.UseCaseFactory, log port.Logger) {
+	metricHandler := handler.NewMetricHandler(ucFactory, log)
+	r.GET("/ping", metricHandler.PingDB)
+	r.GET("/", metricHandler.GetAll)
+	r.POST("/update/", metricHandler.UpdateJSON)
+	r.POST("/updates/", metricHandler.UpdatesJSON)
+	r.POST("/value/", metricHandler.GetJSON)
+	r.POST("/update/:type/:name/:value", metricHandler.Update)
+	r.GET("/value/:type/:name", metricHandler.GetValue)
+}
