d3b16d67ccae9753aa815fca07f4e660d8194dbc
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-18 00:55:46 +0300

    feat(server|adapters): add metric converter
    Added MetricConverter with methods:
    1. ToEntity;
    2. ToModel.

---
+++ metrics_new/app/internal/server/metrics/adapters/repository/file/converter/metric.go
@@ -0,0 +1,16 @@
+package converter
+
+import (
+	"github.com/iPatrushevSergey/metrics/metrics_new/app/internal/server/metrics/adapters/repository/file/model"
+	"github.com/iPatrushevSergey/metrics/metrics_new/app/internal/server/metrics/domain/entity"
+)
+
+//go:generate goverter gen .
+
+// goverter:converter
+// goverter:output:file metric_gen.go
+// goverter:output:package converter
+type MetricConverter interface {
+	ToEntity(source model.Metric) entity.Metric
+	ToModel(source entity.Metric) model.Metric
+}
