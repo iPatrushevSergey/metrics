740a4ef3005bcc1e9cd4e7413de5e22ca3d66bed
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-20 23:00:45 +0300

    feat(server|application): add the usecase of metrics to the factory
    Added usecase:
    1. RecordAuditToFile;
    2. SendAuditRemote.

--- metrics_new/app/internal/server/metrics/application/factory/factory.go
+++ metrics_new/app/internal/server/metrics/application/factory/factory.go
@@ -14,6 +14,9 @@ type MetricUseCasesParams struct {
 	Transactor     port.Transactor
 	MetricFileRepo port.MetricFileRepository
 	SyncFileWrites bool
+	AuditFileRepo  port.AuditFileRepository
+	AuditGateway   port.AuditGateway
+	AuditPublisher port.AuditPublisher
 }
 
 // MetricUseCases holds metrics module use cases exposed to the composition root.
@@ -27,33 +30,45 @@ type MetricUseCases struct {
 	PingDB                 port.UseCase[struct{}, struct{}]
 	MetricsSnapshot        port.UseCase[struct{}, int]
 	RestoreMetricsFromFile port.UseCase[struct{}, struct{}]
+	RecordAuditToFile      port.UseCase[dto.AuditEvent, struct{}]
+	SendAuditRemote        port.UseCase[dto.AuditEvent, struct{}]
 }
 
 // NewMetricUseCases builds metrics module use cases.
 func NewMetricUseCases(p MetricUseCasesParams) *MetricUseCases {
 	var metricFileRepo port.MetricFileRepository
-
 	if p.MetricFileRepo != nil && p.SyncFileWrites {
 		metricFileRepo = p.MetricFileRepo
 	}
 
-	var metricsSnapshot port.UseCase[struct{}, int]
-	var restoreMetricsFromFile port.UseCase[struct{}, struct{}]
-
+	var (
+		metricsSnapshot        port.UseCase[struct{}, int]
+		restoreMetricsFromFile port.UseCase[struct{}, struct{}]
+		recordAuditToFile      port.UseCase[dto.AuditEvent, struct{}]
+		sendAuditRemote        port.UseCase[dto.AuditEvent, struct{}]
+	)
 	if p.MetricFileRepo != nil {
 		metricsSnapshot = usecase.NewMetricsSnapshot(p.MetricRepo, p.MetricFileRepo)
 		restoreMetricsFromFile = usecase.NewRestoreMetricsFromFile(p.MetricRepo, p.MetricFileRepo)
+	}
+	if p.AuditFileRepo != nil {
+		recordAuditToFile = usecase.NewRecordAuditToFile(p.AuditFileRepo)
+	}
+	if p.AuditGateway != nil {
+		sendAuditRemote = usecase.NewSendAuditRemote(p.AuditGateway)
 	}
 
 	return &MetricUseCases{
 		GetMetricValue:         usecase.NewGetMetricValue(p.MetricRepo, p.MetricSvc),
 		GetMetric:              usecase.NewGetMetric(p.MetricRepo, p.MetricSvc),
-		UpdateMetric:           usecase.NewUpdateMetric(p.MetricRepo, p.MetricSvc, metricFileRepo),
-		UpsertMetric:           usecase.NewUpsertMetric(p.MetricRepo, p.MetricSvc, metricFileRepo),
-		UpsertMetricsBatch:     usecase.NewUpsertMetricsBatch(p.MetricRepo, p.MetricSvc, p.Transactor, metricFileRepo),
+		UpdateMetric:           usecase.NewUpdateMetric(p.MetricRepo, p.MetricSvc, metricFileRepo, p.AuditPublisher),
+		UpsertMetric:           usecase.NewUpsertMetric(p.MetricRepo, p.MetricSvc, metricFileRepo, p.AuditPublisher),
+		UpsertMetricsBatch:     usecase.NewUpsertMetricsBatch(p.MetricRepo, p.MetricSvc, p.Transactor, metricFileRepo, p.AuditPublisher),
 		GetAllMetrics:          usecase.NewGetAllMetrics(p.MetricRepo, p.MetricSvc),
 		PingDB:                 usecase.NewPingDB(p.MetricRepo),
 		MetricsSnapshot:        metricsSnapshot,
 		RestoreMetricsFromFile: restoreMetricsFromFile,
+		RecordAuditToFile:      recordAuditToFile,
+		SendAuditRemote:        sendAuditRemote,
 	}
 }
