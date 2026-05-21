464e5d6e51641275f37986c3bcd5086f1681d9c8
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-21 21:49:18 +0300

    feat(server|application): renamed the method from Send to CreateAudit

--- metrics_new/app/internal/server/metrics/application/port/audit_gateway.go
+++ metrics_new/app/internal/server/metrics/application/port/audit_gateway.go
@@ -6,7 +6,7 @@ import (
 	"github.com/iPatrushevSergey/metrics/metrics_new/app/internal/server/metrics/application/dto"
 )
 
-// AuditGateway sends audit events to a remote HTTP endpoint.
+// AuditGateway creates audit events at a remote HTTP endpoint.
 type AuditGateway interface {
-	Send(ctx context.Context, e dto.AuditEvent) error
+	CreateAudit(ctx context.Context, e dto.AuditEvent) error
 }
