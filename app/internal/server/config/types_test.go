3b8055cd12ba157fca8e3e6321585bbca79a212b
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-23 15:07:44 +0300

    tests(server|config): add config types tests
    Added TestAddress_Set, TestAddress_Set_invalid, TestDuration_Set tests.

---
+++ app/internal/server/config/types_test.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAddress_Set(t *testing.T) {
+	var a Address
+	require.NoError(t, a.Set("localhost:8080"))
+	assert.Equal(t, "http", a.Schema)
+	assert.Equal(t, "localhost", a.Host)
+	assert.Equal(t, 8080, a.Port)
+	assert.Equal(t, "http://localhost:8080", a.URL())
+}
+
+func TestAddress_Set_invalid(t *testing.T) {
+	var a Address
+	assert.Error(t, a.Set("localhost"))
+}
+
+func TestDuration_Set(t *testing.T) {
+	var d Duration
+	require.NoError(t, d.Set("5"))
+	assert.Equal(t, 5*time.Second, d.Duration)
+}
