42593f14519804546ef401b86559777c92f65bbe
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-23 20:27:15 +0300

    tests(server|bootstrap): add router test
    Added TestNewRouter test.

---
+++ app/cmd/server/bootstrap/router_test.go
@@ -0,0 +1,24 @@
+package bootstrap
+
+import (
+	"testing"
+
+	"github.com/iPatrushevSergey/metrics/app/internal/pkg/adapters/logger"
+	pkginmemory "github.com/iPatrushevSergey/metrics/app/internal/pkg/adapters/repository/inmemory"
+	metricinmemory "github.com/iPatrushevSergey/metrics/app/internal/server/metrics/adapters/repository/inmemory"
+	"github.com/iPatrushevSergey/metrics/app/internal/server/metrics/domain/service"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewRouter(t *testing.T) {
+	f := NewUseCaseFactory(
+		WithMetricRepo(metricinmemory.NewMetricMemoryRepository()),
+		WithMetricSvc(service.MetricService{}),
+		WithTransactor(pkginmemory.NewTransactor()),
+	)
+
+	r, err := NewRouter(f, logger.NewNopLogger(), "secret", nil)
+	require.NoError(t, err)
+	require.NotNil(t, r)
+}
