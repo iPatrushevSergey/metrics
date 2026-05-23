2ca0e701f9c051645ed913a24fb02cb0d84afbd1
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-23 15:49:54 +0300

    tests(server|application): add update metric use case test
    Added TestUpdateMetric_Execute_update test.

--- app/internal/server/metrics/application/usecase/update_metric_test.go
+++ app/internal/server/metrics/application/usecase/update_metric_test.go
@@ -28,3 +28,19 @@ func TestUpdateMetric_Execute_create(t *testing.T) {
 	require.NoError(t, err)
 	assert.Equal(t, 1.5, *got.Value)
 }
+
+func TestUpdateMetric_Execute_update(t *testing.T) {
+	ctx := context.Background()
+	repo := inmemory.NewMetricMemoryRepository()
+
+	uc := NewUpdateMetric(repo, service.MetricService{}, nil, nil)
+	_, err := uc.Execute(ctx, dto.UpdateMetricInput{MType: "gauge", ID: "cpu", Value: "1"})
+	require.NoError(t, err)
+
+	_, err = uc.Execute(ctx, dto.UpdateMetricInput{MType: "gauge", ID: "cpu", Value: "2"})
+	require.NoError(t, err)
+
+	got, err := repo.GetByID(ctx, "cpu")
+	require.NoError(t, err)
+	assert.Equal(t, 2.0, *got.Value)
+}
