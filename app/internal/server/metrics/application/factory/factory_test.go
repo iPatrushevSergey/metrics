97b38b68d547035140512e200c8b9773dbf109a6
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-23 16:19:20 +0300

    tests(server|application): add factory test
    Added TestNewMetricUseCases_withFileRepo test.

--- app/internal/server/metrics/application/factory/factory_test.go
+++ app/internal/server/metrics/application/factory/factory_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	pkginmemory "github.com/iPatrushevSergey/metrics/app/internal/pkg/adapters/repository/inmemory"
+	"github.com/iPatrushevSergey/metrics/app/internal/server/metrics/adapters/repository/file/metrics"
 	metricinmemory "github.com/iPatrushevSergey/metrics/app/internal/server/metrics/adapters/repository/inmemory"
 	"github.com/iPatrushevSergey/metrics/app/internal/server/metrics/domain/service"
 
@@ -20,4 +21,18 @@ func TestNewMetricUseCases(t *testing.T) {
 	assert.NotNil(t, uc.GetMetric)
 	assert.NotNil(t, uc.UpsertMetricsBatch)
 	assert.NotNil(t, uc.PingDB)
+	assert.NotNil(t, uc.GetAllMetrics)
+	assert.NotNil(t, uc.UpdateMetric)
+}
+
+func TestNewMetricUseCases_withFileRepo(t *testing.T) {
+	fileRepo := metrics.NewMetricFileRepository(t.TempDir() + "/m.json")
+	uc := NewMetricUseCases(MetricUseCasesParams{
+		MetricRepo:     metricinmemory.NewMetricMemoryRepository(),
+		MetricSvc:      service.MetricService{},
+		Transactor:     pkginmemory.NewTransactor(),
+		MetricFileRepo: fileRepo,
+	})
+	assert.NotNil(t, uc.MetricsSnapshot)
+	assert.NotNil(t, uc.RestoreMetricsFromFile)
 }
