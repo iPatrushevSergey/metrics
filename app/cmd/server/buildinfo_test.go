91683dbb2bf71efd45ea4dd1ec40d117434cf950
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-23 20:32:30 +0300

    tests(server): add buildinfo test
    Added TestBuildNA test.

---
+++ app/cmd/server/buildinfo_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBuildNA(t *testing.T) {
+	assert.Equal(t, "N/A", buildNA(""))
+	assert.Equal(t, "1.0", buildNA("1.0"))
+}
