3bd6aa72c10cdd3c88bfb48a288c232de15c4088
S.Patrushev <PatrushevSergeyVal@yandex.ru>
2026-05-23 17:24:59 +0300

    tests(server|application): add get all metrics usecase test
    Added TestGetAllMetrics_Execute_error test.

--- app/internal/server/metrics/application/usecase/get_all_metrics_test.go
+++ app/internal/server/metrics/application/usecase/get_all_metrics_test.go
@@ -2,14 +2,17 @@ package usecase
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/iPatrushevSergey/metrics/app/internal/server/metrics/adapters/repository/inmemory"
+	"github.com/iPatrushevSergey/metrics/app/internal/server/metrics/application/port/mocks"
 	"github.com/iPatrushevSergey/metrics/app/internal/server/metrics/domain/entity"
 	"github.com/iPatrushevSergey/metrics/app/internal/server/metrics/domain/service"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
 )
 
 func TestGetAllMetrics_Execute(t *testing.T) {
@@ -25,4 +28,15 @@ func TestGetAllMetrics_Execute(t *testing.T) {
 	require.Len(t, out, 1)
 	assert.Equal(t, "cpu", out[0].MetricID)
 	assert.Contains(t, out[0].MetricValue, "42")
+}
+
+func TestGetAllMetrics_Execute_error(t *testing.T) {
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	reader := mocks.NewMockMetricReader(ctrl)
+	reader.EXPECT().GetAll(ctx).Return(nil, errors.New("db"))
+
+	uc := NewGetAllMetrics(reader, service.MetricService{})
+	_, err := uc.Execute(ctx, struct{}{})
+	assert.Error(t, err)
 }
